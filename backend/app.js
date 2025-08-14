5277cce0fd7ef2530a7e3bd24b0418eaddfbbc23
Sayuru Dev <mailtosayuru@gmail.com>
2025-08-14 04:26:04 -0400

    mongo connected

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 import express from "express";
 const app = express();
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import { connectDatabase } from "./config/dbConnect.js";
 
 dotenv.config({ path: "backend/config/config.env" });
 
+connectDatabase();
+
+import productRoutes from "./routes/products.js";
+
+app.use("/api/v1", productRoutes);
+
 app.listen(process.env.PORT, () => {
-  console.log(`Server started on PORT: ${process.env.PORT}`);
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
 });
---
+++ backend/config/dbConnect.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDatabase = () => {
+
+  mongoose.connect(process.env.DB_LOCAL_URI).then((con) => {
+    console.log(
+      `MongoDB Database connected with HOST:`
+    );
+  });
+};
+
